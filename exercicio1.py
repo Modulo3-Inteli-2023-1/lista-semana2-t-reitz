6fff9c21698fc44dd7b6adc2da2cb4cc73e1cd66
t-reitz <123952132+t-reitz@users.noreply.github.com>
2023-08-07 09:13:07 -0300

    FINAL

--- exercicio1.py
+++ exercicio1.py
@@ -6,17 +6,21 @@
 
 # Crie a função que será avaliada no exercício aqui
 
-
-
-
+def conta_palavras_unicas(string):
+    unique_words = []
+    words = string.split(" ")
+    for word in words:
+        if word not in unique_words:
+            unique_words.append(word)
+    
+    return len(unique_words)
 
 
 
 # Teste a sua função aqui (caso ache necessário)
 
-
-
-
+a = conta_palavras_unicas("a b c d e f g h i")
+print(a)
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -6,17 +6,21 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def is_anagram(word1, word2):
+    letters1 = list(word1)
+    letters2 = list(word2)
 
-
-
-
+    if sorted(letters1) == sorted(letters2):
+        return True
+    else:
+        return False
 
 
 # Teste a sua função aqui (caso ache necessário)
 
 
-
-
+a = is_anagram("pedra", "padre")
+print(a)
 
 
 
